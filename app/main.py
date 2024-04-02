3dbeaf0925733734c3bde40e9587f523f25f9a76
Shaposhnikov Alex <firstremont@gmail.com>
2024-04-02 21:41:01 +0300

    2april-crash-system

--- app/main.py
+++ app/main.py
@@ -1,37 +1,38 @@
-from worker_db import get_rooms_by_id#, adding_rooms
+from worker_db import get_rooms_by_id, adding_rooms
+import requests
 import asyncio
 
 
 
-# async def add_rooms(data) -> bool:
-#     a = await adding_rooms(data)
-#     return a
-
-# data = {
-#     "id": 13433,
-#     "name_room": "Дом на дереве, Gaular",
-#     "country": "Норвегия",
-#     "bedrooms": "3",
-#     "night_price": 150,
-# }
-
-# asyncio.run(res = add_rooms(data))
-# print(res)
 
 
 
-# async def read_rooms():
-#     data = await get_rooms_by_id(13433)
-#     #print(data)
-#     return
-
-# # id = 13433
-# asyncio.run(read_rooms())
 
 
+
+
+
+## Work at DB
+# Добавляем в базу тестово
+async def add_rooms(data) -> bool:
+    a = await adding_rooms(data)
+    return print(a)
+
+data = {
+    "id": 13434,
+    "name_room": "Дом на дереве, Gaular",
+    "country": "Норвегия",
+    "bedrooms": "3",
+    "night_price": 150,
+}
+
+asyncio.run(add_rooms(data))
+
+
+#  Забираем из базы тестово
 async def read_rooms():
-    id = 13433
+    id = 13434
     data = await get_rooms_by_id(id)
     print(data.country)
 
-asyncio.run(read_rooms())
+# asyncio.run(read_rooms())
