a82774ab3ad75927e8e7869605950d1732dd7f54
Shaposhnikov Alex <firstremont@gmail.com>
2024-04-04 02:18:19 +0300

    4april

--- app/options_chrome.py
+++ app/options_chrome.py
@@ -3,3 +3,4 @@ import random
 # PROFILESS CHROME
 def profil() -> str:
     return "profile" + str(random.randint(1, 10))
+
--- app/scraper.py
+++ app/scraper.py
@@ -5,64 +5,107 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import requests
-# import pandas as pd
 import time
 import random
+# import pandas as pd
 # import re
 
-ua = UserAgent()
+ua = UserAgent(browsers=['edge', 'chrome']) # ua = UserAgent(browsers=['edge', 'chrome'])  ua = UserAgent(os='linux') ua = UserAgent(min_version=120.0)  ua = UserAgent(platforms='mobile')  
 service = Service()
 options = Options()
 
 
-# OPTINONS
-prof = profil()
-url = "https://www.airbnb.com/s/Bali--Indonesia/homes?adults=1&place_id=ChIJoQ8Q6NNB0S0RkOYkS7EPkSQ&refinement_paths%5B%5D=%2Fhomes"
 
+# OPTINONS DRIVER CHRONE SELENIUM
+prof = profil()
+print("\nOPTIONS DRIVER CHROME:")
+####
 def add_options(options, *args):
     for arg in args:
         options.add_argument(arg)
-
-add_options(options, "--disable-webgl", "--disable-gpu", "--disable-3d-apis", "--enable-virtual-keyboard", 
-                    "--mute-audio", "--disable-plugins-discovery", "--profile-directory=Default", "disable-infobars",
-                    "start-maximized", "--disable-blink-features=AutomationControlled")
-
-# options.add_argument('--headless=new') # Режим без окна, старый - options.add_argument('--headless=old)
-options.add_argument(f"--user-agent={ua.random}")
-options.add_argument(f"user-data-dir=./profiles/{prof}/")
+        print(arg)
+####
+add_options(options, "--disable-webgl", "--disable-gpu", "--disable-3d-apis", "--enable-virtual-keyboard", "--mute-audio", "--disable-plugins-discovery", "--profile-directory=Default", "disable-infobars", "start-maximized", "--disable-blink-features=AutomationControlled", f"--user-agent={ua.random}", f"user-data-dir=./profiles/{prof}/") # , "--incognito", f"--proxy-server={PROXY}", "--headless=new")
+####
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option("useAutomationExtension", False)
-#options.add_argument("--incognito")
-#options.add_argument(f"--proxy-server={PROXY}")
-
 driver = webdriver.Chrome(service=service, options=options)
-driver.delete_all_cookies()
+# driver.delete_all_cookies()
 driver.set_window_size(1200,800)
 driver.set_window_position(0,0)
-
-# Получение ответа сервера
-status_code = requests.get(url)
-print(f"HTTP response code: {status_code}")
-print(f"Used profil - {prof}")
-
-page = driver.get(url)
-
-time.sleep(10)
-
-# Получение исходного кода страницы
-html = driver.page_source
-# Создание объекта BeautifulSoup для парсинга HTML
-soup = BeautifulSoup(html, 'lxml')
+####
 
 
-np = soup.find("a", {"aria-label" : "Next"})
-#print(np)
-if np:
-    href = np.get("href")
-    print(href)
-else:
-    print("Link with aria-label 'Next' not found.")
+# QUICK SLEEP - промежуток случайных чисел на входе, между которыми будет случайный период остановки
+def quick_sleep(mi: int, ma: int) -> bool:
+    print("Please wait...")
+    confirm = False
+    time.sleep(random.randint(mi, ma))
+    confirm = True
+    return confirm
 
-time.sleep(300000)
+# RESPONSE CODE URL - на вход url, на выход код ответа сервера
+def response_code(url: str) -> int:
+    response = requests.get(url)
+    code = response.status_code
+    print(f"\nHTTP response code: {code}\n")
+    return code
+
+# GOING TO THE SITE AND GET LXML BS4 - на вход url, переходит на сайт BS4
+def get_url_lxml_bs4(url: str) -> BeautifulSoup:
+    if response_code(url) == 200:
+        quick_sleep(1, 2)
+        driver.get(url) # page = driver.get(url)
+        quick_sleep(1, 2)
+        html = driver.page_source
+        quick_sleep(1, 2)
+        soup = BeautifulSoup(html, 'lxml')
+        quick_sleep(1, 2)
+        return soup
+
+# FIND URL SOUP 
+def find_url_soup(nand, tag, name, value):
+        np = nand.find(tag, {name : value})
+        quick_sleep(1, 2)
+        if np:
+            url_href = np.get("href")
+            return url_href
+        else:
+            print("Error")
+
+
+#quick_sleep(1, 2) # !!!!!! Не знаю почему, но без него не будет работать !!!!!
+
+
+url = "https://www.airbnb.com/s/Bali--Indonesia/homes?adults=1&place_id=ChIJoQ8Q6NNB0S0RkOYkS7EPkSQ&refinement_paths%5B%5D=%2Fhomes"
+
+quick_sleep(3, 4)
+
+soup = get_url_lxml_bs4(url)
+
+quick_sleep(10, 11)
+
+
+a = find_url_soup(soup, "a", "aria-label", "Next")
+print(a)
+
+quick_sleep(100, 200)
+
 driver.close()
 driver.quit()
+
+
+
+
+
+
+
+
+
+# Получение селениумом url
+# page = driver.get(url)
+
+# # Получение исходного кода страницы
+# html = driver.page_source
+# # Создание объекта BeautifulSoup для парсинга HTML
+# soup = BeautifulSoup(html, 'lxml')
